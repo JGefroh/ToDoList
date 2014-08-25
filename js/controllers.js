f5f667b22238ff3063e8d3a079140cb9a60813ca
JGefroh <joseph.gefroh@gmail.com>
2014-08-24 16:13:51 -1000

    Added statistics.

--- js/controllers.js
+++ js/controllers.js
@@ -6,9 +6,12 @@ toDoListApp.controller('ToDoListController', function ($scope, $timeout) {
 
     $scope.TABLE_TASKS_REMAINING = "tasksRemainingView";
     $scope.TABLE_TASKS_COMPLETED = "tasksCompletedView";
+    $scope.STATS = "statsView";
     $scope.currentTable = $scope.TABLE_TASKS_REMAINING;
+    $scope.groupStats = [];
     $scope.tasks = [];
     $scope.tasksFinished = [];
+    $scope.usedGroups = [];
 
     initializePageLeaveWarning();
     updateTimeTracked();
@@ -28,6 +31,10 @@ toDoListApp.controller('ToDoListController', function ($scope, $timeout) {
                 stopTracking(task);
                 startTracking(task);
             }
+        }
+
+        if ($scope.currentTable === $scope.STATS) {
+            updateStats();
         }
 
         var trackingUpdateTimer = $timeout(function () {
@@ -76,6 +83,47 @@ toDoListApp.controller('ToDoListController', function ($scope, $timeout) {
 
     $scope.commitTaskChanges = function(task) {
         $scope.editedTask.name = $scope.editedTaskCopy.name;
+        $scope.editedTask.group = $scope.editedTaskCopy.group;
+    }
+
+    function updateStats() {
+        updateGroups();
+        $scope.groupStats.length = 0;
+        for (index = 0; index < $scope.usedGroups.length; index++) {
+            var groupStat = new Object();
+            groupStat.group = $scope.usedGroups[index];
+            groupStat.taskCount = 0;
+            groupStat.totalTimeTaken = 0;
+            for (index2 = 0; index2 < $scope.tasksFinished.length; index2++) {
+                var currentTask = $scope.tasksFinished[index2];
+                if (currentTask.group === groupStat.group) {
+                    groupStat.taskCount++;
+                    groupStat.totalTimeTaken += currentTask.totalTimeTaken;
+                }
+            }
+            for (index3 = 0; index3 < $scope.tasks.length; index3++) {
+                var currentTask = $scope.tasks[index3];
+                if (currentTask.group === groupStat.group) {
+                    groupStat.taskCount++;
+                    groupStat.totalTimeTaken += currentTask.totalTimeTaken;
+                }
+            }
+            $scope.groupStats.push(groupStat);
+        }
+    }
+    function updateGroups() {
+        $scope.usedGroups.length = 0;
+        for (index=0; index<$scope.tasks.length;index++) {
+            if ($scope.usedGroups.indexOf($scope.tasks[index].group) == -1) {
+                $scope.usedGroups.push($scope.tasks[index].group);
+            }
+        }
+
+        for (index=0; index<$scope.tasksFinished.length;index++) {
+            if ($scope.usedGroups.indexOf($scope.tasksFinished[index].group) == -1) {
+                $scope.usedGroups.push($scope.tasksFinished[index].group);
+            }
+        }
     }
 
     $scope.toggleTracking = function(task) {
@@ -118,6 +166,12 @@ toDoListApp.controller('ToDoListController', function ($scope, $timeout) {
     $scope.showTasksCompleted = function() {
         $scope.currentTable = $scope.TABLE_TASKS_COMPLETED;
     }
+
+    $scope.showStats = function() {
+        updateStats();
+        $scope.currentTable = $scope.STATS;
+    }
+
 
     $scope.convertMSToHours = function(time) {
         return (Math.round(time / 60000) / 60);
