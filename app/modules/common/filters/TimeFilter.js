ea84a9421e7bf40cb8df74cdf064e3ba79d5ac36
JGefroh <joseph.gefroh@gmail.com>
2014-08-31 22:59:29 -1000

    Added hour/time conversion button to the tables.

--- app/modules/common/filters/TimeFilter.js
+++ app/modules/common/filters/TimeFilter.js
@@ -2,12 +2,18 @@
  * Created by Joseph on 8/25/2014.
  */
 (function() {
-    function TimeFilter() {
+    function MSToHoursFilter() {
         return function (timeToConvertInMS, numberDecimalPositions) {
             return ((timeToConvertInMS / 60000) / 60).toFixed(numberDecimalPositions);
         };
     }
+    function MSToMinutesFilter() {
+        return function (timeToConvertInMS, numberDecimalPositions) {
+            return ((timeToConvertInMS / 60000)).toFixed(numberDecimalPositions);
+        };
+    }
     angular
         .module('ToDoList.Filters')
-        .filter('msToHours', TimeFilter);
+        .filter('msToHours', MSToHoursFilter)
+        .filter('msToMinutes', MSToMinutesFilter)
 })();
