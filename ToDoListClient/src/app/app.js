407601ef4b2f84f4a52b0f9d5d000dd4f761699a
JGefroh <joseph.gefroh@gmail.com>
2015-06-02 22:35:20 -1000

    Incremented version number to v2.2.0.

--- ToDoListClient/src/app/app.js
+++ ToDoListClient/src/app/app.js
@@ -21,7 +21,7 @@ angular
 angular
     .module('ToDoList')
     .constant('applicationName', 'ToDoList')
-    .constant('versionNumber', 'v2.1.8');
+    .constant('versionNumber', 'v2.2.0');
 
 angular
     .module('ToDoList')
