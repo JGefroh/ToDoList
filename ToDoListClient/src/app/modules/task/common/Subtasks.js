1e211c34f48b1457669e4c0a6e0c14f64742ddcf
JGefroh <joseph.gefroh@gmail.com>
2015-06-04 21:40:38 -1000

    Made subtasks into directive.

---
+++ ToDoListClient/src/app/modules/task/common/Subtasks.js
@@ -0,0 +1,32 @@
+(function() {
+    function SubtasksDirective() {
+        function SubtasksDirectiveCtrl($scope, $filter,
+                                     TaskService, UserService, AlertService,
+                                     truncateLimit) {
+            var vm = this;
+
+            function initialize() {
+            }
+
+            initialize();
+        }
+        return {
+            restrict: 'A',
+            replace: true,
+            scope: {
+                tasks: '=',
+                disabled: '=ngDisabled',
+                hideButtons: '=?'
+            },
+            templateUrl: 'Subtasks.html',
+            controller: ['$scope', '$filter',
+                         'TaskService', 'UserService', 'AlertService',
+                         'truncateLimit',
+                         SubtasksDirectiveCtrl],
+            controllerAs: 'subtaskCtrl'
+        }
+    }
+    angular
+        .module('ToDoList.TaskModule')
+        .directive('subtasks', [SubtasksDirective]);
+})();
